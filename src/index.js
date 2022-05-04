05dc632804e920aee585bff4b93139388eda731e
Lilia Lomaka <llomaka80@gmail.com>
2022-05-04 22:20:18 +0300

    Запит і виведення в консоль

--- src/index.js
+++ src/index.js
@@ -1,1 +1,74 @@
 import './css/main.css';
+import { Notify } from 'notiflix';
+import { fetchMovies } from './js/fetchMovies';
+import { renderGallery } from './js/renderGallery';
+import getRefs from './js/getRefs';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { addBackToTop } from 'vanilla-back-to-top'
+
+const STORAGE_KEY = 'movie-page-number';
+const STORAGE_SEARCH = 'movie-search-end';
+const refs = getRefs();
+let searchValue = '';
+Notify.init({ fontSize: '18px', timeout: 7000});
+refs.more.classList.add("visually-hidden");
+refs.button.addEventListener('click', onSubmit);
+refs.more.addEventListener('click', onClick);
+addBackToTop();
+
+function onSubmit(event) {
+  event.preventDefault();
+  refs.more.classList.add("visually-hidden");
+  const value = refs.input.value.trim();
+  searchValue = value;
+  refs.gallery.innerHTML = '';
+  localStorage.removeItem(STORAGE_SEARCH);
+  localStorage.removeItem(STORAGE_KEY);
+  if (!value) {
+    return Notify.warning('Please enter some text to input field!');
+  }
+  fetchMovies(value)
+    .then(response => {
+      // refs.gallery.innerHTML = '';
+      console.log(response);
+      // if (response.data.totalHits === 0)
+      //   return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      // Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      // renderGallery(response);
+      // if (response.data.totalHits < 40) {
+      //   Notify.warning("We're sorry, but you've reached the end of search results.");
+      //   localStorage.setItem(STORAGE_SEARCH, 1);
+      // }
+      // if (response.data.totalHits > 40) {
+      //   refs.more.classList.remove("visually-hidden");
+      // }
+    })
+    .catch(onSearchError);
+  event.target.form.reset();
+};
+
+function onClick() {
+  if (localStorage.getItem(STORAGE_SEARCH))
+    return Notify.warning("We're sorry, but you've reached the end of search results.");
+  let pageCounter = 1;
+  if (localStorage.getItem(STORAGE_KEY))
+    pageCounter = localStorage.getItem(STORAGE_KEY);
+  fetchMovies(searchValue, Number(pageCounter) + 1)
+    .then(response => {
+      if (!response.data.hits.length) {
+        localStorage.removeItem(STORAGE_KEY);
+        localStorage.setItem(STORAGE_SEARCH, 1);
+        refs.more.classList.add("visually-hidden");
+        return Notify.warning("We're sorry, but you've reached the end of search results.");
+      }
+      let numbers = response.request.responseURL.match(/\d+/g);
+      localStorage.setItem(STORAGE_KEY, numbers[numbers.length - 1]);
+      renderGallery(response);
+    })
+    .catch(onSearchError);
+}
+
+function onSearchError(error) {
+  Notify.failure(error);
+  console.error(error);
+}
--- src/js/fetchMovies.js
+++ src/js/fetchMovies.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://developers.themoviedb.org/3/trending/get-trending';
-const AUTH_TOKEN = '?api_key=fdf0e898687a376156944fbb1ab25196';
-const PARAMS = '&language=en-US&include_image_language=en,null&media_type=movie&include_adult=true&per_page=40&page=';
+// axios.defaults.baseURL = 'https://developers.themoviedb.org/3/trending/get-trending';
+// const AUTH_TOKEN = '?api_key=fdf0e898687a376156944fbb1ab25196';
+// const PARAMS = '&language=en-US&include_image_language=en,null&media_type=movie&include_adult=true&per_page=40&page=';
 
-export async function fetchPictures(input, pageCounter = 1) {
-  const data = await axios(AUTH_TOKEN + input + PARAMS + pageCounter);
+export async function fetchMovies(input, pageCounter = 1) {
+  const { data } = await axios(`https://api.themoviedb.org/3/search/movie?api_key=fdf0e898687a376156944fbb1ab25196&language=en-US&page=${pageCounter}&include_adult=false&query=${input}`);
   return data;
 }
